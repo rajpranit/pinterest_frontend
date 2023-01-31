64bd303a38410ffd4d8b01c21f678bbe8535586d
rajpranit <rajpranit1207@gmail.com>
2023-01-31 11:17:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Login from "./components/login";
+import Home from "./container/home";
+
+const App = () => {
+  return (
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
+      <Routes>
+        <Route path="login" element={<Login />} />
+
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </GoogleOAuthProvider>
+  );
+};
+
+export default App;
